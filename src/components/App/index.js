e01536727197f135af28ce8d2c1010b011c14add
Sergei Nepryakhin <sergey.nepryahin@dashboquet.com>
2020-08-06 20:05:28 +0300

    feat: add example

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,27 +6,26 @@ import './index.css';
 const App = () => {
   const [count, setCount] = useState(0);
   // const count = 43;
+  // 1
   // const [ obj, setObj] = useState({a: 1, b: 2, c: 3});
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* //1 */}
         {/* <p>Obj value: A: {obj.a} ; B: {obj.b} ; C: {obj.c}</p> */}
         <p>Counter value: {count}</p>
         <div
           className="App-link"
           // onClick={() => setObj({a: 2})}
           // onClick={() => setObj({...obj, a: 2})}
+          // onClick={() => {setCount((count) => count + 1); setCount((count) => count + 1); setCount((count) => count + 1);}}
           onClick={() => setCount(count + 1)}
         >
           Click to +1
         </div>
-        {/* <div
-          className="App-link"
-          onClick={() => setTimeout(()=>alert(count), 5000)} 
-        >
-          Send to event loop
-        </div> */}
+        {/* 
+         */}
       </header>
     </div>
   );
